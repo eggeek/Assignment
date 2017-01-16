d8802a7f48a4bb1fcccbba0864a4f00b3086ca9e
eggeek <eggeek.me@gmail.com>
2017-01-16 12:07:10 +1100

    init discrete optimization

--- Coursera/Discerete_Optimization/knapsack/solverJava.py
+++ Coursera/Discerete_Optimization/knapsack/solverJava.py
@@ -29,10 +29,9 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
-        input_data_file = open(file_location, 'r')
-        input_data = ''.join(input_data_file.readlines())
-        input_data_file.close()
+        with open(file_location, 'r') as input_data_file:
+            input_data = input_data_file.read()
         print solve_it(input_data)
     else:
-        print 'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)'
+        print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
 
